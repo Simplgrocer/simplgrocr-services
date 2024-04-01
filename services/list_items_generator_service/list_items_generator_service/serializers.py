48749abc9620fba0abdd25bb9d371ed0cb070a48
tirthya_kamal_dasgupta <tirthya.dasgupta@alliedmedia.com>
2024-04-01 16:33:57 +0530

    feat: Configured to generate the list items.

--- services/list_items_generator_service/list_items_generator_service/serializers.py
+++ services/list_items_generator_service/list_items_generator_service/serializers.py
@@ -3,3 +3,10 @@ from rest_framework import serializers
 
 class PromptSerializer(serializers.Serializer):
     prompts = serializers.CharField()
+
+
+class ListItemSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    measurementUnit = serializers.ChoiceField(choices=('unit', 'kg', 'g'))
+    quantity = serializers.FloatField()
+    totalPrice = serializers.FloatField()
--- services/list_items_generator_service/list_items_generator_service/views.py
+++ services/list_items_generator_service/list_items_generator_service/views.py
@@ -1,7 +1,16 @@
+import json
+from dotenv import load_dotenv
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import PromptSerializer
+from .serializers import ListItemSerializer, PromptSerializer
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
 
 @api_view(["POST"])
@@ -18,6 +27,8 @@ def generate_list_items(request):
 
     prompts_list = prompts_data.split("\n")
 
-    print(prompts_list)
+    result = llm.invoke(f'Transform a list of items {prompts_list} described in strings into a structured array of objects. Each item string, such as "Chips 1 unit 10/unit", will be converted into an object with specific attributes: "name", "unitOfMeasurement", "quantity", "pricePerUnitOfMeasurement", and "totalPrice". The "name" attribute will identify the item (e.g., "Chips"), the "unitOfMeasurement" will specify the measurement unit (e.g., "unit", "kg", or "g"), the "quantity" will note the amount, and the "pricePerUnitOfMeasurement" will detail the cost per unit of measurement. Finally, calculate the "totalPrice" based on the unit of measurement and the price per unit. For units measured in "unit", the total price should be the product of quantity and price per unit. For items measured in "kg" or "g", the total price calculation will adjust accordingly. Use the provided array of strings as your input and generate the corresponding array of objects as the output. Just produce the raw JSON without any wrappings as the output.')
 
-    return Response({"message": "Request received successfully."}, status=200)
+    print(result)
+
+    return Response(json.loads(result.content), status=200)
